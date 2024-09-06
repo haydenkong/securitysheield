d71bf4acb7fecf3d64aa516dbe3edd2506fee729
PixelVerseIT <115069692+PixelVerseIT@users.noreply.github.com>
2024-09-06 15:41:41 +0930

    return back

--- api/index.js
+++ api/index.js
@@ -113,6 +113,36 @@ app.post('/securityshield/v0/identity/devmode', allowAllOrigins, checkPassword, 
   `);
 });
 
+// API status - general status of whole API
+app.get('/', (req, res) => {
+  res.json({ status: 'Welcome to PixelVerse Systems API.' });
+});
+
+// API status - general status of whole API
+app.get('/ping', (req, res) => {
+  res.json({ status: 'PixelVerse Systems API is up and running. All checks return normal. Please email contact@pixelverse.tech if you experience any errors.' });
+});
+
+// API status - general status of whole API
+app.get('/ping', (req, res) => {
+  res.json({ status: 'PixelVerse Systems API is up and running. All checks return normal. Please email contact@pixelverse.tech if you experience any errors.' });
+});
+
+// Status - see if SecurityShield is active
+app.get('/securityshield/v1/status', checkOrigin, (req, res) => {
+  res.json({ status: 'SecurityShield is currently active.' });
+});
+
+// Log - returns with request info
+app.get('/securityshield/v1/log', (req, res) => {
+  const requestDetails = {
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+  };
+  res.json(requestDetails);
+});
+
 // Google Gemini API Key (restricted access)
 app.post('/securityshield/v1/KJHG88293543', checkOrigin, (req, res) => {
   res.json({ apiKey: 'google-gemini-api' });
